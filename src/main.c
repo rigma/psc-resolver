f48d63c6ad1d09f27a98fa5a003407c280816351
rigma <rigbuntu@gmail.com>
2016-02-12 14:35:08 +0100

    Début du projet Configuration de la solution et Hello World

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	char test[50];
+
+	printf("Hello world !\n");
+	scanf("%s", test);
+	printf("%s\n", test);
+
+	return EXIT_SUCCESS;
+}
