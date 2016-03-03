af7b70d1ae53cce93438c73d96e1ebc97d559fd2
Larthanor <lartragnarock@gmail.com>
2016-03-03 12:55:20 +0100

    Libérée, délivrée, les problèmes peuvent s'en aller

--- src/problem.c
+++ src/problem.c
@@ -46,9 +46,21 @@ problem_t *problem_init(const char *filename) {
     return p;
 }
 
-void problem_free(problem_t *p) {
-    if (p == NULL)
-        return NULL;
+void problem_free(problem_t *p)
+{
+	size_t i = 0;
 
-    
+	if (p == NULL)
+        return NULL;
+	
+	for (; i < p->n_vars; i++)
+		var_free(p->vars[i]);
+	free(p->vars);
+
+	for (i = 0; i < p->n_constraints; i++)
+		constraint_free(p->constraints[i]);
+	free(p->constraints);
+	
+	string_free(p->name);
+	free(p);
 }
